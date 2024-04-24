0ebb5ebc3f51874801484b469ceac1d105a97c2f
Nikolai Maslak <nikolai.maslak@brightpattern.com>
2024-04-24 16:32:48 +0800

    Fix linkedin btn

--- linkedin.meta.js
+++ linkedin.meta.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://*linkedin.com/in/*
 // @grant       none
-// @version     1.4
+// @version     1.5
 // @author      -
 // @description 11/16/2023, 4:01:58 AM
 // @run-at      document-end
